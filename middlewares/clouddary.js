4523782dc7487466687e5803d63a92fdc501d0a5
Ali Raza Yusufzai <114583866+AliYusufzai@users.noreply.github.com>
2023-09-16 01:36:50 +0500

    Add files via upload

---
+++ middlewares/clouddary.js
@@ -0,0 +1,10 @@
+// import {v2 as cloudinary} from 'cloudinary';
+// const cloudinary = require('cloudinary').v2;
+          
+// cloudinary.config({ 
+//   cloud_name: 'dzq1h0xyu', 
+//   api_key: '345126432123499', 
+//   api_secret: 'cqCvcU_hqshoESszVszEnB5-D_8' 
+// });
+
+// module.exports = cloudinary
