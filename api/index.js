cf52c3fff17630aea100402f571491aed3b61a03
Burhan Ali Shaikh <85751219+BurhanShaikh-9@users.noreply.github.com>
2023-09-19 13:10:26 +0500

    Update index.js
    cors added

--- api/index.js
+++ api/index.js
@@ -3,8 +3,10 @@ require("dotenv").config();
 require("./models/db");
 const useRouter = require("./routes/user");
 const User = require("./models/user");
+const cors = require("cors"); 
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(useRouter);
@@ -14,7 +16,7 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is conncet to part number ${PORT}`);
+  console.log(`Server is connected to part number ${PORT}`);
 });
 
 //// mongodb+srv://azam:azam123@imradatabase.lu0ke6f.mongodb.net/imradatabase?retryWrites=true&w=majority
