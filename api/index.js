f7bde436e523eec4d661a96eaeebd8dae9aef7a4
Burhan Ali Shaikh <85751219+BurhanShaikh-9@users.noreply.github.com>
2023-09-19 13:54:45 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -8,8 +8,22 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
+app.use(function (req, res, next) {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 app.use(express.json());
-app.use(useRouter);
+app.use(useRouter);c
 const PORT = 8000;
 app.get("/", (req, res) => {
   res.send("<h1>Hello world<h1>");
