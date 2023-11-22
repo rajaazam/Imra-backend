803b1815b05bf5d0dfb789c1d609e4167b8bb729
Ali Raza Yusufzai <aliyusufzai03@gmail.com>
2023-11-22 16:27:16 +0500

    .

--- api/middlewares/multerConfig.js
+++ api/middlewares/multerConfig.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 var storage = multer.diskStorage({
-    destination: "./api/assets/documents",
+    destination: "./assets/documents",
     filename: (req, file, cb) => {
         //const unique = Date.now() + "-" + Math.round(Math.random() * 1e9);
         cb(null, file.originalname);
